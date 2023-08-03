3f4d05b67b3ace623ed5c2e2390e9f229eecb1bc
Steven Busenbarrick <sbusenba@gmail.com>
2023-08-03 16:41:28 -0700

    add category and item models

---
+++ models/category.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const itemschema = new Schema({
+  name: { type: String },
+  description: { type: String },
+});
+
+itemschema.virtual("url").get(function () {
+  return `/category/${this._id}`;
+});
+
+module.exports = mongoose.model("Item", itemschema);
---
+++ models/item.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const itemschema = new Schema({
+  name: { type: String },
+  description: { type: String },
+  categories: [{ type: Schema.Types.ObjectId }],
+  price: { type: Number },
+  numberinstock: { type: Number },
+});
+
+itemschema.virtual("url").get(function () {
+  return `/item/${this._id}`;
+});
+
+module.exports = mongoose.model("Item", itemschema);
