fa96410d80633600e10e88acb64ed91b54a100d5
Steven Busenbarrick <sbusenba@gmail.com>
2023-08-04 13:03:30 -0700

    add get and post methods for category/add route

--- routes/category.js
+++ routes/category.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var router = express.Router();
+const categoryController = require("../controllers/categorycontroller");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Category" });
 });
+
+router.get("/add", categoryController.add_category_get);
+
+router.post("/add", categoryController.add_category_post);
 
 module.exports = router;
