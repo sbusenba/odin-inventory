8f9cfdfc6fec7254396336730bdce77e3d4f330a
Steven Busenbarrick <sbusenba@gmail.com>
2023-08-11 06:55:28 -0700

    add item/category count to index.pug

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require("express");
 var router = express.Router();
 const itemController = require("../controllers/itemcontroller");
-
+const Item = require("../models/item");
+const Category = require("../models/category");
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get("/", async function (req, res, next) {
+  const [itemCount, categoryCount] = await Promise.all([
+    Item.countDocuments().exec(),
+    Category.countDocuments().exec(),
+  ]);
+  res.render("index", { title: "InventoryTracker", itemCount, categoryCount });
 });
 
 module.exports = router;
